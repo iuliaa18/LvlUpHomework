ac5d083d9911efbd4f177485071b3569fad2f32e
iuliaa18 <pahmutova1806@gmail.com>
2022-10-06 21:28:01 +0300

    до 15/10/22 Задание 2

---
+++ Ex2.py
@@ -0,0 +1,33 @@
+# Задание 2.
+a = int(input("Введите число месяца: "))
+if a == 1:
+    print("ЯНВАРЬ")
+elif a == 2:
+    print("ФЕВРАЛЬ")
+elif a == 3:
+    print("МАРТ")
+elif a == 4:
+    print("АПРЕЛЬ")
+elif a == 5:
+    print("МАЙ")
+elif a == 6:
+    print("ИЮНЬ")
+elif a == 7:
+    print("ИЮЛЬ")
+elif a == 8:
+    print("АВГУСТ")
+elif a == 9:
+    print("СЕНТЯБРЬ")
+elif a == 10:
+    print("ОКТЯБРЬ")
+elif a == 11:
+    print("НОЯБРЬ")
+elif a == 12:
+    print("ДЕКАБРЬ")
+else:
+    print("Некорректный ввод")
+
+
+
+
+
